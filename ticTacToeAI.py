1a0507abbea45f4b0d3393abbeb15503bac8c458
nor5 <noorbelkacem5@gmail.com>
2022-05-23 16:33:41 +0200

    Ai game

--- ticTacToeAI.py
+++ ticTacToeAI.py
@@ -19,8 +19,23 @@ class Board():
                     row[row.index(val)] = letter
                     if self.winner(position, letter):
                        self.current_winner = letter
+
+    def undomove(self,position):
+        
+        sim_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
+        for row in sim_board:     
+
+             for val in row:
                     
-##                    print( f" {letter } has choose  { position}   row[] {row[row.index(position)]}")
+                    if  val ==  position :
+                        i = sim_board.index(row)
+                        j = row.index(val)
+        
+                        self.board[i][j] = position
+
+
+        
+## print( f" {letter } has choose  { position}   row[] {row[row.index(position)]}")
 
     
     def   availablePosition(self):
@@ -30,6 +45,7 @@ class Board():
                 
               if  P in [str(i) for i in range(9)]:
                   #print(f' t is :{t}  and g is: {g}' )
+                  
                   positionList.append(P)
           
         if len(positionList) == 0:
@@ -130,17 +146,17 @@ class smartComputer(Player):
         if board.current_winner  == other_player:
             return {'position':None, 'score':1*(game.availablePosition()['num_empty_squar'] + 1 ) if other_player == max_player
                         else   -1*(board.availablePosition()['num_empty_squar'] +1)}
-        elif  board.availablePosition()['num_empty_squar'] == 0:
+        if  not  board.availablePosition()['num_empty_squar']  :
             return{'position':None, 'score':0}
 
-        if max_player == player:
-            best ={'position':None, 'score':-math.inf}
+        if player == max_player:
+            best ={'position':None, 'score': -math.inf}
         else:
-            best = {'position':None, 'score':math.inf}
+            best = {'position':None, 'score': math.inf}
         for possible_move in board.availablePosition()['availabe position']:
             board.insertLetter(possible_move, player)
             simulate = self.minimax(board, other_player)
-            board.insertLetter(possible_move, possible_move) # undo the move by replacing the letter by the position number
+            board.undomove(possible_move) # undo the move by replacing the letter by the position number
             board.current_winner = None
             simulate['position'] = possible_move
             if player == max_player:
